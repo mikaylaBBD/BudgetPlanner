8a80897b910fa358953a4d7f6f307fcd13236843
raees21 <raeesaziz786@gmail.com>
2022-04-14 12:49:01 +0200

    Models with Gitignore

---
+++ BudgetPlanner/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Models
+{
+  public class Users
+  {
+    [Key]
+    public int ID { get; set; }
+
+    public string User { get; set; }
+  }
+}
