74fc8ec56ab4472b4a2e19c1bcf995e770eb89d6
Raees Aziz Muhammad <raees@bbd.co.za>
2022-04-14 12:32:06 +0200

    Models

---
+++ BudgetPlanner/Models/Transactions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Models
+{
+  public class Transactions
+  {
+    [Key]
+    public int ID { get; set; }
+
+    public decimal Amount { get; set; }
+
+    [ForeignKey("Accounts")]
+    public int AccountID { get; set; }
+
+    [ForeignKey("Goals")]
+    public int GoalID { get; set; }
+
+    [ForeignKey("Category")]
+    public int CategoryID { get; set; }
+
+    [ForeignKey("Users")]
+    public int UserID { get; set; }
+  }
+}
---
+++ BudgetPlanner/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Models
+{
+  public class Users
+  {
+    public int ID { get; set; }
+
+    public string User { get; set; }
+  }
+}
