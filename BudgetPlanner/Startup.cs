8646d1be0380d1e1c40586e32fe662f8205e7194
raees21 <raeesaziz786@gmail.com>
2022-04-17 13:56:51 +0200

    Auth0 Login System Completed

--- BudgetPlanner/Startup.cs
+++ BudgetPlanner/Startup.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,11 +28,74 @@ namespace BudgetPlanner
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
+      services.AddRazorPages();
+
+      // Add authentication services
+      services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+      })
+      .AddCookie()
+      .AddOpenIdConnect("Auth0", options => {
+              // Set the authority to your Auth0 domain
+              options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+              // Configure the Auth0 Client ID and Client Secret
+              options.ClientId = Configuration["Auth0:ClientId"];
+        options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+              // Set response type to code
+              options.ResponseType = OpenIdConnectResponseType.Code;
+
+              // Configure the scope
+              options.Scope.Clear();
+        options.Scope.Add("openid");
+
+              // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+              // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+              options.CallbackPath = new PathString("/callback");
+
+              // Configure the Claims Issuer to be Auth0
+              options.ClaimsIssuer = "Auth0";
+
+        options.Events = new OpenIdConnectEvents
+        {
+                // handle the logout redirection
+                OnRedirectToIdentityProviderForSignOut = (context) =>
+          {
+            var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+            var postLogoutUri = context.Properties.RedirectUri;
+            if (!string.IsNullOrEmpty(postLogoutUri))
+            {
+              if (postLogoutUri.StartsWith("/"))
+              {
+                      // transform to absolute
+                      var request = context.Request;
+                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+              }
+              logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+            }
+
+            context.Response.Redirect(logoutUri);
+            context.HandleResponse();
+
+            return Task.CompletedTask;
+          }
+        };
+      });
+
+      // Add framework services.
+      services.AddControllersWithViews();
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseAuthentication();
+      app.UseAuthorization();
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
