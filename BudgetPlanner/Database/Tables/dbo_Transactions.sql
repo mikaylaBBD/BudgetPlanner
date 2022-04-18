294c5990a271a9b0c35b7a70928c670df6e0f83b
TiaanTerblanche <tiaant@bbd.co.za>
2022-04-18 13:08:46 +0200

    Update dbo_Transactions.sql

--- BudgetPlanner/Database/Tables/dbo_Transactions.sql
+++ BudgetPlanner/Database/Tables/dbo_Transactions.sql
@@ -5,8 +5,8 @@ GO
 CREATE TABLE [dbo].[Transactions](
 	[transactionID] [INT] NOT NULL,
 	[transactionAmount] [DECIMAL] (18,2) NOT NULL,
-	[accountID] [INT] FOREIGN KEY REFERENCES  [dbo].[Accounts](id),
-	[categoriesID] [INT] FOREIGN KEY REFERENCES  [dbo].[Categories](id),
+	[accountID] [INT] FOREIGN KEY REFERENCES  [dbo].[Accounts](accountID),
+	[categoriesID] [INT] FOREIGN KEY REFERENCES  [dbo].[Categories](categoryID),
 	[userID] [INT] FOREIGN KEY REFERENCES  [dbo].[Users](userID),
 	[transactionDate] [DATE] NOT NULL
 	PRIMARY KEY CLUSTERED ([transactionID])
