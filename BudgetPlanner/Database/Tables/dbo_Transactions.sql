5795b4597803e93541bfb9cc5845b37447d296ff
TiaanTerblanche <tiaant@bbd.co.za>
2022-04-18 13:07:09 +0200

    Update dbo_Transactions.sql

--- BudgetPlanner/Database/Tables/dbo_Transactions.sql
+++ BudgetPlanner/Database/Tables/dbo_Transactions.sql
@@ -3,11 +3,12 @@ GO
  DROP TABLE IF EXISTS dbo.[Transactions]
 
 CREATE TABLE [dbo].[Transactions](
-	[id] [INT] NOT NULL,
-	[amount] [DECIMAL] (18,2) NOT NULL,
-	[account] [INT] FOREIGN KEY REFERENCES  [dbo].[Accounts](id),
-	[categories] [INT] FOREIGN KEY REFERENCES  [dbo].[Categories](id),
-	[user] [VARCHAR](200) FOREIGN KEY REFERENCES  [dbo].[Users](token)
-	PRIMARY KEY CLUSTERED ([id])
+	[transactionID] [INT] NOT NULL,
+	[transactionAmount] [DECIMAL] (18,2) NOT NULL,
+	[accountID] [INT] FOREIGN KEY REFERENCES  [dbo].[Accounts](id),
+	[categoriesID] [INT] FOREIGN KEY REFERENCES  [dbo].[Categories](id),
+	[userID] [INT] FOREIGN KEY REFERENCES  [dbo].[Users](userID),
+	[transactionDate] [DATE] NOT NULL
+	PRIMARY KEY CLUSTERED ([transactionID])
 )
 	
