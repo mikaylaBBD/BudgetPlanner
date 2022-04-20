278720361998643337fbfbbe7977fd6bfb2d2697
MikaylaBBD <mikaylan@bbd.co.za>
2022-04-20 11:05:57 +0200

    transactions controller

---
+++ BudgetPlanner/Controllers/TransactionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BudgetPlanner.Database;
+using BudgetPlanner.Models;
+
+namespace BudgetPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly DBManager _context;
+
+        public TransactionsController(DBManager context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Transactions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transactions>>> GetTransactions()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        // GET: api/Transactions/[id] : api/Transactions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transactions>> GetTransactions(int id)
+        {
+            var transactions = await _context.Transactions.FindAsync(id);
+
+            if (transactions == null)
+            {
+                return NotFound();
+            }
+
+            return transactions;
+        }
+
+        // PUT: api/Transactions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTransactions(int id, Transactions transactions)
+        {
+            if (id != transactions.TransactionID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(transactions).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransactionsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Transactions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Transactions>> PostTransactions(Transactions transactions)
+        {
+            _context.Transactions.Add(transactions);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTransactions", new { id = transactions.TransactionID }, transactions);
+        }
+
+        // DELETE: api/Transactions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTransactions(int id)
+        {
+            var transactions = await _context.Transactions.FindAsync(id);
+            if (transactions == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transactions.Remove(transactions);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TransactionsExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionID == id);
+        }
+    }
+}
